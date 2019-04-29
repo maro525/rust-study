e193c744c3ec8fb7f22d6c48096e8763cda09117
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2019-04-29 22:53:30 +0900

    random function

--- guessing/src/main.rs
+++ guessing/src/main.rs
@@ -1,14 +1,22 @@
+extern crate rand;
+
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess String::new();
+    println!("The secret number is :{}", secret_number);
 
-    io::stdin().read_line(&mut guess)
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println("You guessed: {}", guess);
+    println!("You guessed: {}", guess);
 }
